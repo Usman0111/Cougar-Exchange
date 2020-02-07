6142a9ceb56372a34391d564177b32154c9c8a73
cod1r <jasonho353@gmail.com>
2020-02-06 20:06:34 -0600

    added nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,10 @@ class App extends Component {
   render() {
     return (
 	   <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+	     <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
@@ -25,11 +28,10 @@ class App extends Component {
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
+
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
     </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
+   
   </Navbar.Collapse>
 </Navbar> 
      );
