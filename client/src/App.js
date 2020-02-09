300dfd452d895726765d7c5b03ff4fb0d2b353c9
Muhammad Usman <muhammadusman0200@gmail.com>
2020-02-08 23:17:10 -0600

    fixed bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/AppNavbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-    <Navbar></Navbar>
-  </div>;
-  )
+      <Navbar></Navbar>
+    </div>
+  );
 }
 
 export default App;
