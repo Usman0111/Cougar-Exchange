07e762575d980c801ebdc3e2b5693e6c883c799f
cod1r <jasonho353@gmail.com>
2020-02-21 18:02:18 -0600

    fixed gitignore

--- backend/sqlconnect.js
+++ backend/sqlconnect.js
@@ -1,14 +1,14 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  // host: "bcuvaa603ia6qja1utcx-mysql.services.clever-cloud.com",
-  // user: "uhihayslnl9k5tfu",
-  // password: "Oa1zgUk4whPeaCYld738",
-  // database: "bcuvaa603ia6qja1utcx"
-  // host: "localhost",
-  // user: "root",
-  // password: "root",
-  // database: "exchange"
+   host: "bcuvaa603ia6qja1utcx-mysql.services.clever-cloud.com",
+   user: "uhihayslnl9k5tfu",
+   password: "Oa1zgUk4whPeaCYld738",
+   database: "bcuvaa603ia6qja1utcx"
+ //  host: "localhost",
+ //  user: "root",
+ //  password: "root",
+ //  database: "exchange"
 });
 
 module.exports = db;
